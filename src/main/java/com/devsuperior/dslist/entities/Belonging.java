0eedf75009afa1b03151f559dd1b9cb7ce8cffb5
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-08-19 17:58:22 -0300

    Domain Model

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+    
+    @EmbeddedId
+    private belongingPK id = new belongingPK();
+    private Integer position;    
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList list, Integer position) {
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+    
+
+}
